eef5ff6c01cbd0d05b9e3e4458fc2b7632c1d3e9
Louis Leonardo <leonardolouis2@gmail.com>
2021-03-05 19:02:46 +0700

    adding how to print hello world with PHP and HTML tag

---
+++ byfreeCodeCamp/PHP-hello_world.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // cara I
+        echo "Hello World </br>";
+
+    // cara II
+        echo("Hello World 2");
+
+    // -------------
+    echo "<h1>Heading 1 with php</h1>";
+    ?>
+</body>
+</html>
