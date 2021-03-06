b44dc154dea25bff9c8372541b5644e94555cf4d
Louis Leonardo <leonardolouis2@gmail.com>
2021-03-06 18:47:43 +0700

    how to calcaulate in php

---
+++ byfreeCodeCamp/numCalculation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo abs(-300).'</br>'; //bkin angka jadi positif / absolute
+        echo pow(2,3).'</br>'; //bkin pangkat dalam angka php 
+        echo sqrt(144).'</br>'; //bkin akar
+        echo max(8,92).'</br>'; //cari angka paling besar
+        echo min(50,-25).'</br>'; //cari angka paling kecil
+        echo round(3.8).'</br>'; //pembulatan
+        echo ceil(2.3).'</br>'; //pembulatan ke atas
+        echo floor(2.9).'</br>'; //pembulatan ke bawah
+    ?>
+</body>
+</html>
